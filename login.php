0d483cda7f988b201c2bb09d25ee419da1d17d0b
seyeonsim <sysim@ucalgary.ca>
2022-11-26 15:13:25 -0700

    bug fix

--- login.php
+++ login.php
@@ -12,11 +12,11 @@ if(isset($_POST['submit'])){
     $filter_pass = filter_var(md5($_POST['pass']), FILTER_SANITIZE_STRING);
     $pass = mysqli_real_escape_string($conn, $filter_pass);
  
-    $select= mysqli_query($conn, "SELECT * FROM `customer` WHERE email = '$email' AND password = '$pass'") or die('query failed');
+    $select= mysqli_query($conn, "SELECT * FROM `customer` WHERE Email = '$email' AND Password = '$pass'") or die('query failed');
 
     if(mysqli_num_rows($select) > 0){
         $row = mysqli_fetch_assoc($select);
-        $_SESSION['user_id'] = $row['id'];
+        $_SESSION['user_id'] = $row['ID'];
         header('location:index.php');
      }else{
         $message[] = 'Incorrect username or password!';
